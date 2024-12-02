e34e4b3bc5338065d6f9e51aeaaf8471de437601
Matthew Hellmer <theprankmonkey@gmail.com>
2024-12-01 22:07:50 -0500

    Can create items completely now.

--- fe/app/src/components/CreateItem.js
+++ fe/app/src/components/CreateItem.js
@@ -8,7 +8,11 @@ const CreateItem = () => {
   const [roomId, setRoomId] = useState("");
   const [chestId, setChestId] = useState("");
   const [formData, setFormData] = useState({
-    chest: "",
+    name: "",
+    pocket_id: "",
+    quantity: 0,
+    upc: "",
+    notes: "",
   });
 
   const bubbleUpRooms = (value) => {
@@ -23,21 +27,104 @@ const CreateItem = () => {
       pocket_id: value,
     });
   };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const payload = formData;
+      console.log("Sending Payload: ", payload);
+      const response = await fetch(`http://127.0.0.1:5123/items/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      //TODO Update Banner
+      //TODO Clear some fields like name, quantity, UPC, and notes. Leave room/chest/pocket.
+    } catch (error) {
+      // Handle errors (e.g., display an error message)
+      console.error("Error submitting form:", error);
+    }
+  };
 
   return (
-    <>
-      <ListRooms bubbleUp={bubbleUpRooms} />
-      {roomId ? (
-        <ListRoomChests bubbleUp={bubbleUpRoomChests} roomId={roomId} />
-      ) : (
-        "Please select a room first"
-      )}
-      {chestId ? (
-        <ListChestPockets bubbleUp={bubbleUpChestPockets} chestId={chestId} />
-      ) : (
-        "Please select a chest first"
-      )}
-    </>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter a name:
+        <input
+          type="text"
+          value={formData.name}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              name: e.target.value,
+            })
+          }
+        />
+      </label>
+      <label>
+        Enter a quantity:
+        <input
+          type="text"
+          value={formData.quantity}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              quantity: e.target.value,
+            })
+          }
+        />
+      </label>
+      <label>
+        Select a room:
+        <ListRooms bubbleUp={bubbleUpRooms} />
+      </label>
+      <label>
+        Select a chest:
+        {roomId ? (
+          <ListRoomChests bubbleUp={bubbleUpRoomChests} roomId={roomId} />
+        ) : (
+          <p>"Please select a room first"</p>
+        )}
+      </label>
+      <label>
+        Select a pocket:
+        {chestId ? (
+          <ListChestPockets bubbleUp={bubbleUpChestPockets} chestId={chestId} />
+        ) : (
+          <p>"Please select a chest first"</p>
+        )}
+      </label>
+      <label>
+        Enter a UPC:
+        <input
+          type="text"
+          value={formData.upc}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              upc: e.target.value,
+            })
+          }
+        />
+      </label>
+      <label>
+        Enter notes:
+        <textarea
+          value={formData.notes}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              notes: e.target.value,
+            })
+          }
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
