5577a44dfb26a1043766eec868d0b5f2ac878d93
Matthew Hellmer <theprankmonkey@gmail.com>
2025-01-02 02:38:31 -0500

    Had to add a timestamp to the call or browsers won't refresh image.

--- fe/app/src/components/ItemImageBlobRenderer.js
+++ fe/app/src/components/ItemImageBlobRenderer.js
@@ -8,9 +8,10 @@ const ItemImageBlobRenderer = ({ item }) => {
   useEffect(() => {
     const fetchImage = async () => {
       try {
-        console.log("Trying to grab image for item ", item);
+        console.log("Trying to grab image for item", item.id, item.image_file);
+        const timestamp = new Date().getTime();
         const response = await fetch(
-          `${config.IMAGE_URL}/items/${item.id}/photo`
+          `${config.IMAGE_URL}/items/${item.id}/photo?t=${timestamp}`
         );
         const blob = await response.blob();
         const imageUrl = URL.createObjectURL(blob);
@@ -19,13 +20,18 @@ const ItemImageBlobRenderer = ({ item }) => {
         console.error("Error fetching image:", error);
       }
     };
-
     fetchImage();
   }, []);
+
   return (
     <div>
       {imageUrl && (
-        <img src={imageUrl} alt="Fetched Image" width="75" height="75" />
+        <img
+          src={imageUrl}
+          alt={`Fetched Image for item ${item.id}`}
+          width="75"
+          height="75"
+        />
       )}
     </div>
   );
